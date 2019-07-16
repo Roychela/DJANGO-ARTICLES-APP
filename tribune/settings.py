39be16f1bfea6ac19588f2a2b8167bc3757ec429
Roy Chela <roychela@gmail.com>
2019-07-16 14:27:15 +0300

    Deploy new

--- tribune/settings.py
+++ tribune/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
+DEBUG=False
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mtr8bune.herokuapp.com']
 
 
 # Application definition
